7bc5eb3b1caf65079093be2e3b2c9c7cff978918
Байбак Кирилл <zloifox@gmail.com>
2022-03-15 14:39:27 +0300

    Созданы models

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,1 +1,50 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+
+
+class User(AbstractUser):
+    pass
+    # Изменить setting -> AUTH_USER_MODEL = 'reviews.User'
+    # username =
+    # email =
+    # role =
+    # bio =
+    # first_name =
+    # last_name =
+    
+
+class Category(models.Model):
+    pass
+    # name =
+    # slug =
+
+
+class Comment(models.Model):
+    pass
+    # review_id, title_id, comment_id
+    # text =
+    # author =
+    # pub_date =
+
+
+class Genre(models.Model):
+    pass
+    # name =
+    # slug =
+
+
+class Review(models.Model):
+    pass
+    # title_id
+    # text =
+    # score =
+    # author =
+    # pub_date =
+
+
+class Title(models.Model):
+    pass
+    # name =
+    # year =
+    # category =
+    # genre = ?
