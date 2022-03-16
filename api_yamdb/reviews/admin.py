b71ac3e20879c4e5c67cb524cfe49772bc1f31f7
Байбак Кирилл <zloifox@gmail.com>
2022-03-16 13:50:52 +0300

    изменения admin.py, добавлены поля поиска и фильтрации

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -2,53 +2,57 @@ from django.contrib import admin
 from .models import User, Category, Comment, Genre, Review, Title
 
 
-# class UserAdmin(admin.ModelAdmin):
-#     list_display = ('username',)
-#         'username', 'email', 'role', 'bio', 'first_name', 'last_name'
-#     )
-#     search_fields = ('',)
-#     list_filter = ('',)
-#     empty_value_display = '-пусто-'
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'username', 'bio'
+    )
+    search_fields = ('username',)
+    list_filter = ('username',)
+    empty_value_display = '-пусто-'
 
 
-# class CategoryAdmin(admin.ModelAdmin):
-#     list_display = ('name', 'slug')
-#     search_fields = ('',)
-#     list_filter = ('',)
-#     empty_value_display = '-пусто-'
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    search_fields = ('name',)
+    list_filter = ('name',)
+    empty_value_display = '-пусто-'
 
 
-# class CommentAdmin(admin.ModelAdmin):
-#     list_display = ('text', 'author', 'pub_date')
-#     # search_fields = ('',)
-#     # list_filter = ('',)
-#     empty_value_display = '-пусто-'
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('id',)
+    # search_fields = ('',)
+    # list_filter = ('',)
+    empty_value_display = '-пусто-'
 
 
-# class GenreAdmin(admin.ModelAdmin):
-#     list_display = ('name', 'slug')
-#     search_fields = ('',)
-#     list_filter = ('',)
-#     empty_value_display = '-пусто-'
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    search_fields = ('name',)
+    list_filter = ('name',)
+    empty_value_display = '-пусто-'
 
 
-# class ReviewAdmin(admin.ModelAdmin):
-#     list_display = ('text', 'score', 'author', 'pub_date')
-#     # search_fields = ('',)
-#     # list_filter = ('',)
-#     empty_value_display = '-пусто-'
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = (
+        'title', 'author', 'text', 'score', 'rev_pub_date'
+    )
+    search_fields = ('author', 'title', 'rev_pub_date')
+    list_filter = ('author', 'title', 'rev_pub_date')
+    empty_value_display = '-пусто-'
 
 
-# class TitleAdmin(admin.ModelAdmin):
-#     list_display = ('name', 'year', 'category', 'genre', 'description')
-#     search_fields = ('',)
-#     list_filter = ('',)
-#     empty_value_display = '-пусто-'
+class TitleAdmin(admin.ModelAdmin):
+    list_display = (
+        'name', 'year', 'category', 'genre', 'description'
+    )
+    search_fields = ('name', 'year', 'category', 'genre')
+    list_filter = ('name', 'year', 'category', 'genre')
+    empty_value_display = '-пусто-'
 
 
-admin.site.register(User)  # UserAdmin)
-admin.site.register(Category)  # CategoryAdmin)
-admin.site.register(Comment)  # CommentAdmin)
-admin.site.register(Genre)  # GenreAdmin)
-admin.site.register(Review)  # ReviewAdmin)
-admin.site.register(Title)  # TitleAdmin)
+admin.site.register(User, UserAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Comment, CommentAdmin)
+admin.site.register(Genre, GenreAdmin)
+admin.site.register(Review, ReviewAdmin)
+admin.site.register(Title, TitleAdmin)
