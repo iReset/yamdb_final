cc173bc21d20a55094ff5cede56d3da771a8ec0c
Мозговой Валерий <mangotim@yandex.ru>
2022-03-16 22:18:01 +0300

    readonly

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -25,6 +25,7 @@ class ReviewSerializer(serializers.ModelSerializer):
     class Meta:
         model = Review
         fields = '__all__'
+        read_only_fields = ('title',)
 
 
 class TitleSerializer(serializers.ModelSerializer):
