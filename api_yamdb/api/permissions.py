bab5216e5b626c363a03da38867d896590165cb4
Мозговой Валерий <mangotim@yandex.ru>
2022-03-17 12:38:01 +0300

    пермишен

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -1,10 +1,14 @@
 from rest_framework import permissions
 
 
+class Admin(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and request.user.is_admin
+
+
 class AdminOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
-        if request.method in permissions.SAFE_METHODS:
-            return True
-        if request.user.is_authenticated:
-            return request.user.is_superuser  # ???
-        return False
+        return (
+            request.method in permissions.SAFE_METHODS
+            or request.user.is_authenticated and request.user.is_admin
+        )
