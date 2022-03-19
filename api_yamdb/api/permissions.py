a27ee371340b4157dd0b671ffa743d844151441d
Мозговой Валерий <mangotim@yandex.ru>
2022-03-19 16:18:52 +0300

    исправление ошибок

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -19,6 +19,10 @@ class AdminOrReadOnly(permissions.BasePermission):
 
 
 class AuthorModeratorAdminOrReadOnly(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return (request.method in permissions.SAFE_METHODS
+                or request.user.is_authenticated)
+
     def has_object_permission(self, request, view, obj):
         return (
             request.method in permissions.SAFE_METHODS
