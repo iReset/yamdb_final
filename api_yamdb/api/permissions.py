1dffeded27df407910b41110afc3665265712e56
iReset <Reset2010@yandex.ru>
2022-03-20 05:14:47 +0300

    docs: remove incorrect FIXME
    The authenticated check is needed to avoid accessing the is_admin field
    of an unauthenticated user. He does not have this field.

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -3,7 +3,6 @@ from rest_framework import permissions
 
 class Admin(permissions.BasePermission):
     def has_permission(self, request, view):
-        # FIXME: нет смысла проверять authenticated
         return request.user.is_authenticated and (
             request.user.is_admin or request.user.is_superuser)
 
