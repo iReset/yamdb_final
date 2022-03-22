6368348e5b6005ff3ed8f8f6644b8a88c9631ee8
iReset <Reset2010@yandex.ru>
2022-03-22 14:05:56 +0300

    fix(api): replace datetime with timezone

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,8 +1,7 @@
-from datetime import datetime
-
 from django.contrib.auth.models import AbstractUser
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
+from django.utils import timezone
 
 
 class User(AbstractUser):
@@ -119,7 +118,7 @@ class Title(models.Model):
         'Год выпуска',
         validators=[
             MinValueValidator(1),
-            MaxValueValidator(datetime.now().year)
+            MaxValueValidator(timezone.now().year)
         ]
     )
     category = models.ForeignKey(
