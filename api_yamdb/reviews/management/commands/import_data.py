b8e2ffae39a7e8c1053eb19eb7c235ac648fe73b
iReset <Reset2010@yandex.ru>
2022-03-23 03:44:55 +0300

    fix: management command import_data

--- api_yamdb/reviews/management/commands/import_data.py
+++ api_yamdb/reviews/management/commands/import_data.py
@@ -3,13 +3,16 @@ import csv
 from django.conf import settings
 from django.core.management.base import BaseCommand
 
-from reviews.models import Category, Genre, Title, User
+from reviews.models import Category, Comment, Genre, Review, Title, User
 
 Models = {
     User: 'users.csv',
     Category: 'category.csv',
+    Title: 'titles.csv',
     Genre: 'genre.csv',
-    Title: 'titles_v1.csv',
+    Review: 'review.csv',
+    Comment: 'comments.csv',
+    Title.genre.through: 'genre_title.csv',
 }
 
 
@@ -21,10 +24,8 @@ class Command(BaseCommand):
         yes = ['Y', 'y']
         no = ['N', 'n']
         if answer in yes:
-            User.objects.all().delete()
-            Category.objects.all().delete()
-            Genre.objects.all().delete()
-            Title.objects.all().delete()
+            for model, csv_files in Models.items():
+                model.objects.all().delete()
         elif answer in no:
             return ('Лучше воспользоваться admin панелью')
         elif answer not in yes or no:
